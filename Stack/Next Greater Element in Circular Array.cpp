4f20c056b673b867e0388b9eb9d868ed8473d70e
IshanNaikele <ishannaikele23@gmail.com>
2026-02-14 18:22:23 +0530

    Solved Next Greater Element in Circular Array

---
+++ Stack/Next Greater Element in Circular Array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    vector<int> nextGreater(vector<int> &arr) 
+    {
+        // code here
+        int n=arr.size();
+        vector<int>ans(n,-1);
+        stack<int>st;
+        for(int i=0;i<2*n-1;i++)
+        {
+            while(!st.empty() && arr[st.top()]<arr[i%n])
+            {
+                ans[st.top()]=arr[i%n];
+                st.pop();
+            }
+            st.push(i%n);
+        }
+        return ans;
+    }
+};
