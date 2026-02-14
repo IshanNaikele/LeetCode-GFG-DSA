8bf4a8a2d11d28d6e951ec30de63bf52d941bccb
IshanNaikele <ishannaikele23@gmail.com>
2026-02-14 14:23:15 +0530

    Solved by two Approaches

--- Stack/921. Minimum Add to Make Parentheses Valid.cpp
+++ Stack/921. Minimum Add to Make Parentheses Valid.cpp
@@ -1,3 +1,5 @@
+//Approach 1 :
+
 class Solution {
 public:
     int minAddToMakeValid(string s) 
@@ -13,5 +15,24 @@ public:
             }
         }
         return st.size();
+    }
+};
+
+//Approach 2 :
+class Solution {
+public:
+    int minAddToMakeValid(string s) 
+    {
+        int open=0,close=0;
+        for(char ch:s)
+        {
+            if(ch=='(') open++;
+            else
+            {
+                if(open>0) open--;
+                else       close++; 
+            }
+        }
+        return open + close;
     }
 };
