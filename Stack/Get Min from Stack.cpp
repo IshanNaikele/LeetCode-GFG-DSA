1418300ec0d13c9dd36747dfd4f38658e38b1f6c
IshanNaikele <ishannaikele23@gmail.com>
2026-02-22 23:18:56 +0530

    Solved Get Min from Stack

--- Stack/Get Min from Stack.cpp
+++ Stack/Get Min from Stack.cpp
@@ -56,3 +56,60 @@ class SpecialStack {
 
 //Approach 2 :
 
+#include<stack>
+class SpecialStack {
+    stack<long long>st;
+    long long mod=1e9+1;
+    long long minValue=LONG_MAX;
+    public:
+    SpecialStack() 
+    {
+        
+    }
+
+    void push(int x) 
+    {
+         // Add an element to the top of Stack
+        if(x<minValue) minValue=x;
+        long long num=x*mod+minValue;
+        st.push(num);
+    }
+
+    void pop() 
+    {
+        // Remove the top element from the Stack
+        if(!st.empty())
+        {
+            st.pop();
+            if(!st.empty())
+            minValue=st.top()%mod;
+            else 
+            minValue=LONG_MAX;
+        }
+        else 
+        minValue=LONG_MAX;
+    }
+
+    int peek() 
+    {
+        // Returns top element of the Stack
+        if(st.empty()) return -1;
+        return st.top()/mod;
+    }
+
+    bool isEmpty() 
+    {
+        // Check if stack is empty
+        return st.size()==0;
+    }
+
+    int getMin() 
+    {
+        // Finds minimum element of Stack
+        if(st.empty()) return -1;
+        
+        minValue=st.top()%mod;
+        
+        return minValue;
+    }
+};
