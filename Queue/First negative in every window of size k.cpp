4e4fbde13c22127da01cc31e940109f29fc36dad
IshanNaikele <ishannaikele23@gmail.com>
2026-02-25 02:06:24 +0530

    Solved First negative in every window of size k

--- Queue/First negative in every window of size k.cpp
+++ Queue/First negative in every window of size k.cpp
@@ -20,3 +20,35 @@ class Solution {
         return ans;
     }
 };
+
+//Approach 2 :
+
+class Solution {
+  public:
+    vector<int> firstNegInt(vector<int>& arr, int k) 
+    {
+        // write code here
+        int n=arr.size();
+        vector<int>ans(n-k+1);
+        queue<int>q;
+        
+        for(int i=0;i<k-1;i++) 
+        {
+            if(arr[i]<0)
+            q.push(i);
+        }
+        
+        for(int i=k-1;i<n;i++)
+        {
+            if(arr[i]<0)
+            q.push(i);
+            while(!q.empty() && q.front()<i-k+1) q.pop();
+            if(!q.empty())
+            {
+                int index=q.front();
+                ans[i-k+1]=arr[index];
+            }
+        }
+        return ans;
+    }
+};
