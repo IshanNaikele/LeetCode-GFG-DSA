c22e3bb9d1ccbeb9b346121a2910f9beb325af7a
IshanNaikele <ishannaikele23@gmail.com>
2026-03-01 16:16:37 +0530

    Solved Insertion at doubly linked list

--- Linked List/Insertion at doubly linked list.cpp
+++ Linked List/Insertion at doubly linked list.cpp
@@ -36,3 +36,42 @@ class Solution {
         return head;
     }
 };
+
+
+//Approach 2 :Same Approach but different Code 
+
+/*
+class Node {
+  public:
+    int data;
+    Node* next;
+    Node* prev;
+
+    Node(int x) {
+        data = x;
+        next = prev = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+    Node *insertAtPos(Node *head, int p, int x) 
+    {
+        // code here
+        Node *temp=head;
+        while(p--)
+        temp=temp->next;
+        
+        Node *newNode=new Node(x);
+         
+        newNode->next=temp->next;
+        newNode->prev=temp;
+        if(temp->next != NULL)
+            temp->next->prev = newNode;
+        
+        temp->next=newNode;
+ 
+        return head;
+    }
+};
