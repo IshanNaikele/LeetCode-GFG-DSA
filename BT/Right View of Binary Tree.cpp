9dfa3d8d7cc5a1832e798444139a009715d04842
IshanNaikele <ishannaikele23@gmail.com>
2026-03-02 18:35:12 +0530

    Solved Right View of Binary Tree

--- BT/Right View of Binary Tree.cpp
+++ BT/Right View of Binary Tree.cpp
@@ -40,3 +40,42 @@ class Solution {
         return ans;
     }
 };
+
+
+//Approach 2 :Using Recursion 
+
+/*
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+    void RView(Node *root,vector<int>&ans,int level)
+    {
+        if(root==NULL) return ;
+        if(ans.size()==level) ans.push_back(root->data);
+        level++;
+        RView(root->right,ans,level);
+        RView(root->left,ans,level);
+    }
+    vector<int> rightView(Node *root) 
+    {
+        //  code here
+        vector<int>ans;
+        if(root==NULL) return ans;
+        int level=0;
+        RView(root,ans,level);
+        return ans;
+    }
+};
