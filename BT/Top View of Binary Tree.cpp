d91b981a93416de414481dd36efbb087257daa73
IshanNaikele <ishannaikele23@gmail.com>
2026-03-03 00:08:38 +0530

    Solved Top View of Binary Tree

--- BT/Top View of Binary Tree.cpp
+++ BT/Top View of Binary Tree.cpp
@@ -67,3 +67,58 @@ class Solution {
         return ans;
     }
 };
+
+//Approach 2 :Recursive Version 
+
+/*
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+    void find(Node *root,int index,int &l,int &r)
+    {
+        if(root==NULL) return;
+        l=min(l,index);
+        r=max(r,index);
+        find(root->left,index-1,l,r);
+        find(root->right,index+1,l,r);
+    }
+    
+    void TView(Node *root,vector<int>&ans,vector<int>&store,int index,int level)
+    {
+        if(root==NULL) return ;
+        if(store[index]>level)
+        {
+            ans[index]=root->data;
+            store[index]=level;
+        }
+        
+        TView(root->left,ans,store,index-1,level+1);
+        TView(root->right,ans,store,index+1,level+1);
+    }
+    vector<int> topView(Node *root) 
+    {
+        // code here
+        if(root==NULL) return {};
+        int left=0,right=0,pos=0,level=0;
+        find(root,pos,left,right);
+        
+        vector<int>ans(right-left+1);
+        vector<int>store(right-left+1,INT_MAX);
+        
+        TView(root,ans,store,abs(left),level);
+        return ans;
+    }
+};
