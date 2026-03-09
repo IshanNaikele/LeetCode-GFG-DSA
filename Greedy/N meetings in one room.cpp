2aa9ef20351128f5d99c8b123cdf916ab8d90c34
IshanNaikele <ishannaikele23@gmail.com>
2026-03-09 20:04:21 +0530

    Solved N meetings in one room

---
+++ Greedy/N meetings in one room.cpp
@@ -0,0 +1,28 @@
+class Solution {
+  public:
+    // Function to find the maximum number of meetings that can
+    // be performed in a meeting room.
+    int maxMeetings(vector<int>& start, vector<int>& end) 
+    {
+        // Your code here
+        vector<pair<int,int>>timer;
+        for(int i=0;i<end.size();i++)
+        timer.push_back({start[i],end[i]});
+        
+        sort(timer.begin(), timer.end(), [](const pair<int,int>& a, const pair<int,int>& b){
+    return a.second < b.second;
+});
+        int total=0;
+        int ending=-1;
+        for(int i=0;i<timer.size();i++)
+        {
+            if(timer[i].first>ending)
+            {
+                total++;
+                ending=timer[i].second;
+            }
+        }
+        
+        return total;
+    }
+};
