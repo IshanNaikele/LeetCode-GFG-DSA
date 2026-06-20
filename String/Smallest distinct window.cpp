76c10a0e1aa6204320b2ec03aa6ba0dbcd032f25
IshanNaikele <ishannaikele23@gmail.com>
2026-06-20 15:27:42 +0530

    Solved Smallest distinct window

--- String/Smallest distinct window.cpp
+++ String/Smallest distinct window.cpp
@@ -1,56 +1,45 @@
 class Solution {
-  public:
-    int findSubString(string& s) 
-    {
-        // code here
-        int n=s.size();
-        int first=0,second=0,minLen=n;
-        
-        vector<int>smallAlpha(26,0);
-        int diff=0;
-        while(first<n)
-        {
-            if(smallAlpha[s[first]-'a']==0)
-            diff++;
-            
-            smallAlpha[s[first]-'a']++;
-            first++;
+public:
+    int findSubString(string& str) {
+
+        vector<int> present(26,0);
+
+        int required = 0;
+
+        for(char ch : str) {
+            if(present[ch-'a']==0)
+                required++;
+            present[ch-'a']++;
         }
-        
-        first=0;
-        for(int i=0;i<26;i++)
-        smallAlpha[i]=0;
-        
-        while(second<n)
-        {
-            while(diff!=0  && second<s.size())
-            {
-                if(smallAlpha[s[second]-'a']==0)
-                diff--;
-                
-                smallAlpha[s[second]-'a']++;
-                second++;
+
+        vector<int> freq(26,0);
+
+        int formed = 0;
+        int left = 0;
+        int ans = INT_MAX;
+
+        for(int right=0; right<str.size(); right++) {
+
+            int idx = str[right]-'a';
+
+            if(freq[idx]==0)
+                formed++;
+
+            freq[idx]++;
+
+            while(formed==required) {
+
+                ans = min(ans, right-left+1);
+
+                freq[str[left]-'a']--;
+
+                if(freq[str[left]-'a']==0)
+                    formed--;
+
+                left++;
             }
-            
-            minLen=min(minLen,second-first);
-            
-            
-            while(diff!=1   )
-            {
-                minLen=min(minLen,second-first);
-                
-                smallAlpha[s[first]-'a']--;
-                
-                if(smallAlpha[s[first]-'a']==0)
-                diff++;
-                
-                first++;
-            }
-            
         }
-        
-        return minLen;
-        
-    
+
+        return ans;
     }
-};                                         
+};
