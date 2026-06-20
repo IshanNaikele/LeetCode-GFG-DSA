dd5c8bcffc274428e0efc0f990787765276e7a83
IshanNaikele <ishannaikele23@gmail.com>
2026-06-20 18:55:10 +0530

    Solved Longest Prefix Suffix

---
+++ String/Longest Prefix Suffix.cpp
@@ -0,0 +1,34 @@
+class Solution 
+{
+  public:
+  
+    int longestPrefixSuffix(string &s)
+    {
+        int n=s.size();
+        vector<int>lps(n,0);
+        
+        int prefix=0,suffix=1;
+        
+        while(suffix<n)
+        {
+            if(s[prefix]==s[suffix])
+            {
+                lps[suffix]=prefix+1;
+                prefix++;
+                suffix++;
+            }
+            else
+            {
+                if(prefix==0) suffix++;
+                else          prefix=lps[prefix-1];
+            }
+        }
+        return lps[n-1];
+    }
+    
+    int getLPSLength(string &s) 
+    {
+        // code here
+        return longestPrefixSuffix(s);
+    }
+};
