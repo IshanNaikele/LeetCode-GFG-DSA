9f1e8d8536a4de80fc0c350e0dd2eba0a03e9feb
IshanNaikele <ishannaikele23@gmail.com>
2026-06-25 19:31:14 +0530

    Solved Add Number Linked Lists

--- Linked List/Add Number Linked Lists.cpp
+++ Linked List/Add Number Linked Lists.cpp
@@ -65,3 +65,113 @@ class Solution {
     } 
 };
 
+/*
+class Node {
+public:
+    int data;
+    Node *next;
+
+    Node(int x) {
+        data = x;
+        next = nullptr;
+    }
+};
+*/
+
+class Solution
+{
+public:
+
+    Node* reverseList(Node* head)
+    {
+        Node *prev=NULL;
+
+        while(head)
+        {
+            Node *next=head->next;
+            head->next=prev;
+            prev=head;
+            head=next;
+        }
+
+        return prev;
+    }
+
+    int length(Node *head)
+    {
+        int cnt=0;
+
+        while(head)
+        {
+            cnt++;
+            head=head->next;
+        }
+
+        return cnt;
+    }
+
+    Node* addTwoLists(Node* head1, Node* head2)
+    {
+        while(head1 && head1->data==0)
+            head1=head1->next;
+
+        while(head2 && head2->data==0)
+            head2=head2->next;
+
+        if(!head1) return new Node(0);
+        if(!head2) return head1;
+
+        int n=length(head1);
+        int m=length(head2);
+
+        // Use longer list as result container
+        if(m>n)
+            swap(head1,head2);
+
+        head1=reverseList(head1);
+        head2=reverseList(head2);
+
+        Node *ans=head1;
+
+        Node *prev=NULL;
+
+        int carry=0;
+
+        while(head1 || head2 || carry)
+        {
+            int sum=carry;
+
+            if(head1)
+                sum+=head1->data;
+
+            if(head2)
+                sum+=head2->data;
+
+            if(head1)
+            {
+                head1->data=sum%10;
+
+                prev=head1;
+
+                head1=head1->next;
+            }
+            else
+            {
+                prev->next=new Node(sum%10);
+                prev=prev->next;
+            }
+
+            if(head2)
+                head2=head2->next;
+
+            carry=sum/10;
+        }
+
+        ans=reverseList(ans);
+
+        while(ans->next && ans->data==0)
+            ans=ans->next;
+
+        return ans;
+    }
+};
