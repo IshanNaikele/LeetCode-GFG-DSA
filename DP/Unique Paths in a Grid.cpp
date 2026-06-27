1b9b0e1c958b0932ffda52a379b5dc3efa167798
IshanNaikele <ishannaikele23@gmail.com>
2026-06-28 03:11:35 +0530

    Solved Unique Paths in a Grid with Memoization

--- DP/Unique Paths in a Grid.cpp
+++ DP/Unique Paths in a Grid.cpp
@@ -25,3 +25,32 @@ class Solution
         return solve(grid,dp,0,0);
     }
 };
+
+//Memoization 
+class Solution 
+{
+  public:
+    int solve(vector<vector<int>> &grid ,vector<vector<int>>&dp ,int row,int col)
+    {
+        int n=grid.size(),m=grid[0].size(),totalPaths=0;
+        if(row==n-1 && col==m-1 ) return 1;
+        if(dp[row][col]!=-1) return dp[row][col];
+        if(grid[row][col]==0)
+        {
+            if(row+1<n)
+            totalPaths+=solve(grid,dp,row+1,col);
+            if(col+1<m)
+            totalPaths+=solve(grid,dp,row,col+1);
+        }
+        
+        return dp[row][col]=totalPaths;
+    }
+    int uniquePaths(vector<vector<int>> &grid) 
+    {
+        // code here
+        int n=grid.size(),m=grid[0].size();
+        if(grid[0][0]==1 || grid[n-1][m-1]==1) return 0;
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        return solve(grid,dp,0,0);
+    }
+};
