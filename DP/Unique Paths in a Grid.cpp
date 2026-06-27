364a9fe230ec5ebb544be44b284f78d75fa963eb
IshanNaikele <ishannaikele23@gmail.com>
2026-06-28 03:15:08 +0530

    Solved Unique Paths in a Grid with Space Optimized Approach

--- DP/Unique Paths in a Grid.cpp
+++ DP/Unique Paths in a Grid.cpp
@@ -91,3 +91,45 @@ class Solution
         return dp[0][0];
     }
 };
+
+//Space Optimization 
+class Solution 
+{
+  public:
+     
+    int uniquePaths(vector<vector<int>> &grid) 
+    {
+        // code here
+        int n=grid.size(),m=grid[0].size();
+        if(grid[0][0]==1 || grid[n-1][m-1]==1) return 0;
+       
+        vector<int>prev(m,0);
+        prev[0]=1;
+        for(int i=0;i<n;i++)
+        {
+            vector<int>temp(m,0);
+            for(int j=0;j<m;j++)
+            {
+                if(i==0 && j==0) 
+                {
+                    temp[j]=1;
+                    continue;
+                }
+                if(grid[i][j]==1) 
+                {
+                    temp[j]=0;
+                    continue;
+                }
+                int up=0,left=0;
+                if(i>0)
+                up=prev[j];
+                if(j>0)
+                left=temp[j-1];
+                
+                temp[j]=up+left;
+            }
+            prev=temp;
+        }
+        return prev[m-1];
+    }
+};
