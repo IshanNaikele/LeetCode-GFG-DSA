cf4af3ab883633604cef8fef5825833fa6506a19
IshanNaikele <ishannaikele23@gmail.com>
2026-06-28 03:14:01 +0530

    Solved Using Tabulation Unique Paths in a Grid

--- DP/Unique Paths in a Grid.cpp
+++ DP/Unique Paths in a Grid.cpp
@@ -54,3 +54,40 @@ class Solution
         return solve(grid,dp,0,0);
     }
 };
+
+//Tabulation 
+class Solution 
+{
+  public:
+     
+    int uniquePaths(vector<vector<int>> &grid) 
+    {
+        // code here
+        int n=grid.size(),m=grid[0].size();
+        if(grid[0][0]==1 || grid[n-1][m-1]==1) return 0;
+        vector<vector<int>>dp(n,vector<int>(m,0));
+        dp[n-1][m-1]=1;
+       
+        for(int i=n-1;i>=0;i--)
+        {
+            
+            for(int j=m-1;j>=0;j--)
+            {
+                if(i==n-1 && j==m-1) continue;
+                if(grid[i][j]==1) 
+                {
+                    dp[i][j]=0;
+                    continue;
+                }
+                int right=0,down=0;
+                if(i+1<n)
+                down=dp[i+1][j];
+                if(j+1<m)
+                right=dp[i][j+1];
+                
+                dp[i][j]=down+right;
+            }
+        }
+        return dp[0][0];
+    }
+};
