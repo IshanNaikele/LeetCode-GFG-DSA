4da8b8cf80c3c4734bc105633a02e26f9548a421
IshanNaikele <ishannaikele23@gmail.com>
2026-07-01 01:38:59 +0530

    Solved 1463. Cherry Pickup II by Tabulation

--- DP/1463. Cherry Pickup II.cpp
+++ DP/1463. Cherry Pickup II.cpp
@@ -70,3 +70,55 @@ public:
         return solve(grid,dp,0,0,n-1);
     }
 };
+
+//Tabulation 
+
+class Solution 
+{
+public:
+
+    int cherryPickup(vector<vector<int>>& grid) 
+    {
+        int m=grid.size(),n=grid[0].size();
+        vector<vector<vector<int>>> dp(m,vector<vector<int>>(n,vector<int>(n,0)));
+
+        for(int j1=0;j1<n;j1++)
+        {
+            for(int j2=0;j2<n;j2++)
+            {
+                if(j1==j2)
+                dp[m-1][j1][j2]=grid[m-1][j1];
+                else
+                dp[m-1][j1][j2]=grid[m-1][j1]+grid[m-1][j2];
+            }
+        }
+        
+
+        for(int i=m-2;i>=0;i--)
+        {
+            for(int j1=0;j1<n;j1++)
+            {
+                for(int j2=0;j2<n;j2++)
+                {
+                    int maxCherries=0; 
+                    for(int dj1=-1;dj1<2;dj1++)
+                        {
+                            for(int dj2=-1;dj2<2;dj2++)
+                            {
+                                int value=0;
+                                if(j1+dj1<0 || j1+dj1>=n || j2+dj2<0 || j2+dj2>=n ) continue;
+                                if(j1==j2) value=grid[i][j1];
+                                else       value=grid[i][j1]+grid[i][j2];
+
+                                value+=dp[i+1][j1+dj1][j2+dj2];
+
+                                maxCherries=max(maxCherries,value);
+                            }
+                        }
+                    dp[i][j1][j2]=maxCherries;
+                }
+            }
+        }
+        return  dp[0][0][n-1] ;
+    }
+};
