8d08e9d53f31826d3067f251c5bfda43f9b4aef6
IshanNaikele <ishannaikele23@gmail.com>
2026-07-03 15:07:49 +0530

    Solved 1823. Find the Winner of the Circular Game

--- Recursion/1823. Find the Winner of the Circular Game.cpp
+++ Recursion/1823. Find the Winner of the Circular Game.cpp
@@ -10,3 +10,86 @@ public:
         return winner(n,k)+1;
     }
 };
+
+//Using Linked List 
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node
+{
+    public:
+    int data;
+    Node *next;
+    Node(int value)
+    {
+        data=value;
+        next=NULL;
+    }
+};
+
+Node *createLL(vector<int>& nums,Node *head)
+{
+    Node *temp=head;
+    int n=nums.size();
+    for(int i=0;i<n;i++)
+    {
+        Node *newNode=new Node(nums[i]);
+        if(head==NULL)
+        {
+            head=newNode;
+            temp=head;
+        }
+        else
+        {
+            temp->next=newNode;
+            temp=temp->next;
+        }
+    }
+    
+    temp->next=head;
+    return temp;
+}
+
+int findWinner(int k,int n,Node *last)
+{
+    Node *temp=last;
+    
+    while(n>1)
+    {
+        int moves = (k - 1) % n;
+        for(int i=0;i<moves;i++)
+          temp=temp->next;
+        
+        Node *del=temp->next;
+        if(del==last) last=temp;
+        temp->next=del->next;
+        delete del;
+        n--;
+    }
+    int ans = temp->data;
+    delete temp;
+    return ans;
+}
+int main()
+{
+    vector<int>nums={1,2,3,4,5};
+    int n=nums.size(),k=2;
+    //Get the last Node
+    Node *last=createLL(nums,NULL);
+    Node *head=last->next;
+    Node *temp=head;
+    //Checking Circular list formed properly ora not
+    
+    for(int i=0;i<n*2;i++)
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    
+    cout<<endl;
+    cout<<findWinner(k,n,last);
+    
+    return 0;
+}
