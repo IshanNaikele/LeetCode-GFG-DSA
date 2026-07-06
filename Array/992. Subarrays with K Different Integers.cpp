441e73fd28881d3a2a6aa843d1c4e3d7244e04a7
IshanNaikele <ishannaikele23@gmail.com>
2026-07-06 19:27:51 +0530

    Solved 992. Subarrays with K Different Integers

--- Array/992. Subarrays with K Different Integers.cpp
+++ Array/992. Subarrays with K Different Integers.cpp
@@ -24,4 +24,54 @@ public:
     {
         return atMostKDistinct(nums,k)-atMostKDistinct(nums,k-1);
     }
-};    
+};
+
+
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+
+int atMostKDistinct(vector<int>&nums,int k)
+{
+    int n=nums.size();
+    int left=0,distinct=0;
+
+    int count=0;
+    unordered_map<int,int>mpp;
+    for(int right=0;right<n;right++)
+    {
+        if(mpp[nums[right]]==0) {
+            distinct++;
+            cout<<"At right index "<<right<<" distinct is "<<distinct<<endl;
+        }
+        mpp[nums[right]]++;
+        while(distinct>k)
+        {
+            cout<<"Entered into while loop "<<endl;
+            mpp[nums[left]]--; 
+            if(mpp[nums[left]]==0) distinct--;
+            left++;
+            cout<<"Left becomes "<<left<<endl;
+        }
+
+        cout<<right-left+1<<" being added in the count "<<endl;
+        count+=(right-left+1);
+        cout<<"Total Count "<<count<<endl;
+    }
+    return count;
+}
+
+int subarraysWithKDistinct(vector<int>& nums, int k) 
+{
+    return atMostKDistinct(nums,k)-atMostKDistinct(nums,k-1);
+}
+
+int main()
+{
+    vector<int>nums={1,2,1,2,3};
+    int k=2;
+    // cout<<atMostKDistinct(nums,k)<<endl;
+    atMostKDistinct(nums,k-1) ;
+    return 0;
+}
