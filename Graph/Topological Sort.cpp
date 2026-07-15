a30ff6357f7fb5efd17c4ed9b91c83fb204c6141
IshanNaikele <ishannaikele23@gmail.com>
2026-07-16 04:21:09 +0530

    Solved Topological Sort

--- Graph/Topological Sort.cpp
+++ Graph/Topological Sort.cpp
@@ -46,3 +46,57 @@ class Solution {
         return ans;
     }
 };
+
+class Solution {
+  public:
+    
+    vector<int> bfs(vector<vector<int>>&adj,vector<int>&visited,vector<int>&indegree)
+    {
+        int V=adj.size();
+        queue<int>q;
+        for(int i=0;i<V;i++) 
+        {
+            if(indegree[i]==0) 
+            {
+                q.push(i);
+                visited[i]=1;
+            }
+        }
+        vector<int>ans;
+        while(!q.empty())
+        {
+            int src=q.front();
+            ans.push_back(src);
+            q.pop();
+            
+            for(int node:adj[src])
+            {
+                indegree[node]--;
+                if(indegree[node]==0 && visited[node]==0) {
+                    visited[node]=1;
+                    q.push(node);
+                }
+            }
+        }
+        return ans;
+    }
+    
+    
+    vector<int> topoSort(int V, vector<vector<int>>& edges) 
+    {
+        // code here
+        vector<vector<int>>adj(V);
+        vector<int>indegree(V,0);
+        for(int i=0;i<edges.size();i++)
+        {
+            int u=edges[i][0];
+            int v=edges[i][1];
+            indegree[v]++;
+            adj[u].push_back(v);
+        }
+         
+        vector<int>visited(V,0);
+        vector<int>ans=bfs(adj,visited,indegree);
+        return ans;
+    }
+};
