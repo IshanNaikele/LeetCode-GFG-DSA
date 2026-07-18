721527968a19f797ea22d2b72fc4c90dc42c2b25
IshanNaikele <ishannaikele23@gmail.com>
2026-07-18 15:56:55 +0530

    Solved Using BFS 1091. Shortest Path in Binary Matrix

--- Graph/1091. Shortest Path in Binary Matrix.cpp
+++ Graph/1091. Shortest Path in Binary Matrix.cpp
@@ -46,3 +46,46 @@ public:
         return (ans==INT_MAX)?-1:ans;
     }
 };
+
+
+class Solution {
+public:
+    int solve(vector<vector<int>>&grid,vector<vector<int>>&visited,int row,int col)
+    {
+        int m=grid.size(),n=grid[0].size();
+        queue<pair<int,pair<int,int>>>q;
+        q.push({1,{row,col}});
+        visited[row][col]=1;
+        int ans=INT_MAX;
+        while(!q.empty())
+        {
+            auto p=q.front();
+            q.pop();
+            int steps=p.first;
+            int row=p.second.first;
+            int col=p.second.second;
+            if(row==m-1 && col==n-1) ans=min(ans,steps);
+             
+            for(int i=-1;i<=1;i++)
+            {
+                for(int j=-1;j<=1;j++)
+                {
+                    if(row+i<m && col+j<n && row+i>=0 && col+j>=0 && grid[row+i][col+j]==0 && visited[row+i][col+j]==0)
+                    {
+                        q.push({steps+1,{row+i,col+j}});
+                        visited[row+i][col+j]=1;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) 
+    {
+        int m=grid.size(),n=grid[0].size();
+        if(grid[0][0]==1 || grid[m-1][n-1]==1) return -1;
+        vector<vector<int>>visited(m,vector<int>(n,0));
+        int ans=solve(grid,visited,0,0);
+        return (ans==INT_MAX)?-1:ans;
+    }
+};
