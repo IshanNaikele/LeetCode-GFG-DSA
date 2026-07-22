9f17dca6204e2236e1654ab18a4a2dc8b54e87fe
IshanNaikele <ishannaikele23@gmail.com>
2026-07-23 02:33:53 +0530

    Solved 45. Jump Game II

--- Greedy/45. Jump Game II.cpp
+++ Greedy/45. Jump Game II.cpp
@@ -24,3 +24,26 @@ public:
         return solve(nums,0,dp);
     }
 };
+
+//Greedy 
+class Solution {
+public:
+     
+    int jump(vector<int>& nums) 
+    {
+        int n=nums.size();
+        int left=0,right=0;
+        int jumps=0,farthest=0;
+
+        while(right<n-1)
+        {
+            for(int i=left;i<=right;i++)
+                farthest=max(farthest,nums[i]+i);
+
+            jumps++;
+            left=right+1;
+            right=farthest;
+        }
+        return jumps;
+    }
+};
