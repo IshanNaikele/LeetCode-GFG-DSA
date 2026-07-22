1d901b10dbfcd70b5fcc3191f8328d1707222656
IshanNaikele <ishannaikele23@gmail.com>
2026-07-23 03:51:06 +0530

    Solved Job Sequencing Problem

--- Greedy/Job Sequencing Problem.cpp
+++ Greedy/Job Sequencing Problem.cpp
@@ -1,3 +1,41 @@
+//Brute Force 
+class Solution {
+  public:
+    vector<int> jobSequencing(vector<int> &deadline, vector<int> &profit) 
+    {
+        // code here
+        vector<vector<int>>vec;
+        int n=profit.size();
+        int maxDeadline=0;
+        for(int i=0;i<n;i++){
+            vec.push_back({profit[i],deadline[i]});
+            maxDeadline=max(maxDeadline,deadline[i]);
+        }
+        sort(vec.rbegin(),vec.rend());
+        vector<int>visited(maxDeadline+1,-1);
+        
+        int totalProfit=0,maxJobs=0;
+        for(int i=0;i<n;i++)
+        {
+            int pr=vec[i][0],t=vec[i][1];
+            
+            for(int i=t;i>0;i--)
+            {
+                if(visited[i]==-1)
+                {
+                    totalProfit+=pr;
+                    maxJobs+=1;
+                    visited[i]=1;
+                    break;
+                }
+            }
+            
+        }
+        
+        return {maxJobs,totalProfit};
+    }
+};
+
 class Solution {
   public:
     vector<int> jobSequencing(vector<int> &deadline, vector<int> &profit) {
